f195e8707b193e63b4fc0d2e89742acbe547bfbf
DensetsuNoNinjin <112174396+DensetsuNoNinjin@users.noreply.github.com>
2022-08-26 20:41:53 +0900

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+//ボタンアクション
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+  if (myselect.value == 0) {
+    document.location.href = "1v1.html";
+  } else if (myselect.value == 1) {
+    document.location.href = "2v2.html";
+  } else if (myselect.value == 2) {
+    document.location.href = "3v3.html";
+  } else if (myselect.value == 3) {
+    document.location.href = "4v4.html";
+  } else if (myselect.value == 4) {
+    document.location.href = "5v5.html";
+  } else {
+    window.alert('人数を選択してください。');
+  }
+});
